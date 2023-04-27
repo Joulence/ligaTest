ad212fda29ccf7a76cba5cdc5717d68538f89aa2
Joulence <4erviken@gmail.com>
2023-04-27 19:03:58 +0300

    Updated Hamburger menu

--- src/js/modules/menu.js
+++ src/js/modules/menu.js
@@ -1,21 +1,18 @@
 export function onclick() {
   const openBtn = document.getElementById('openMenu');
-  const closeBtn = document.getElementById('closeBtn');
+  const logo = document.getElementById('logo');
 
   openBtn.addEventListener('click', () => {
 
     if (document.getElementById("Sidenav").style.width == "250px") {
       document.getElementById("Sidenav").style.width = "0";
       document.getElementById("body").style.marginLeft = "0";
+      logo.style.marginLeft = "0";
     } else {
       document.getElementById("Sidenav").style.width = "250px";
-      document.getElementById("body").style.marginLeft = "250px";
+      document.getElementById("body").style.marginLeft = "265px";
+      logo.style.marginLeft = "250px";
     }
 
-  });
-
-  closeBtn.addEventListener('click', () => {
-    document.getElementById("Sidenav").style.width = "0";
-    document.getElementById("body").style.marginLeft = "0";
   });
 }
