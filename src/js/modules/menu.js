172bdbe8931f5e050372c506c83fe264fa8c83e7
Joulence <4erviken@gmail.com>
2023-04-27 18:27:01 +0300

    Added hamburger menu

--- src/js/modules/menu.js
+++ src/js/modules/menu.js
@@ -1,24 +1,21 @@
 export function onclick() {
-    let hamMenu = document.getElementById("hamMenu");
-    let navMenu = document.getElementById("nav")
-    hamMenu.addEventListener("click", btnPressed);
-    hamMenu.addEventListener("click", showMenu);
+  const openBtn = document.getElementById('openMenu');
+  const closeBtn = document.getElementById('closeBtn');
 
-    function btnPressed() {
-        let isHavMenu = (hamMenu.classList.length > 1) ?
-        hamMenu.classList.remove("is-open") :
-        hamMenu.classList.add("is-open");
-        return isHavMenu;
-      }
+  openBtn.addEventListener('click', () => {
 
-      function showMenu() {
-        let x = document.getElementById("myLinks");
-        if (x.style.display === "block") {
-          x.style.display = "none";
-          navMenu.style.background = "";
-        } else {
-          x.style.display = "block";
-          navMenu.style.background = "#fff";
-        }
-      }
+    if (document.getElementById("Sidenav").style.width == "250px") {
+      document.getElementById("Sidenav").style.width = "0";
+      document.getElementById("body").style.marginLeft = "0";
+    } else {
+      document.getElementById("Sidenav").style.width = "250px";
+      document.getElementById("body").style.marginLeft = "250px";
+    }
+
+  });
+
+  closeBtn.addEventListener('click', () => {
+    document.getElementById("Sidenav").style.width = "0";
+    document.getElementById("body").style.marginLeft = "0";
+  });
 }
