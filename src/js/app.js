ee705e30f7bfbba0b1c1f1b6e44bebd853d97f7c
Joulence <4erviken@gmail.com>
2023-04-28 15:21:54 +0300

    Created accordion

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,10 @@
 import * as isWebP from "./modules/isWeb.js";
 import * as menu from "./modules/menu.js";
+import * as accordion from "./modules/accordion.js"; 
 // import * as date from "./modules/date.js";
 
 
 isWebP.isWebP();
 menu.onClick();
+accordion.toggle();
 // date.thisDate();
